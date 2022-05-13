6fa1dac9c11a0cbc26895485187629ba426dabe1
martinet <yo>
2022-05-13 11:18:10 +0200

    commentaire

--- src/Modele/Jeu.java
+++ src/Modele/Jeu.java
@@ -56,14 +56,21 @@ public class Jeu {
     }
 
     //dans cette méthode on considère que la pièce est dans la grille et ne superpose aucune autre piece (grace à estPosable appellé avant)
-
     public void jouerPiece(int idJoueur,int idPiece, LinkedList<Case> listeCasesPiece){
+            //recupere liste des pieces disponibles pour la couleur courante
             Piece piece = listeJoueurs[idJoueur-1].getCouleurCourante().getPieceDispo(idPiece);
+            //si la piece que l'on veut jouer est disponible
             if(piece != null) {
+                //si la piece est posable selon les regles du jeu
                 if (estPosableRegle(listeCasesPiece, idJoueur)) {
+                    //on ajoute la piece au niveau (plateau du jeu)
                     n.ajouterPiece(piece,listeCasesPiece,getNumCouleurCourante());
+                    //on calcule les coins possibles pour la couleur courante du joueur
                     calculeCoinPiece(piece,idJoueur);
+                    //on joue la piece pour le joueur (et sa couleur courante)
                     listeJoueurs[idJoueur-1].jouePiece(piece);
+                    //si la couleur courante du joueur n'a plus de pieces a jouer
+                    //alors on passe la couleur courante en fin de jeu (peut plus jouer et score final mis à jour)
                     if(!restePieceJouable()){
                         finCouleur();
                     }
@@ -78,27 +85,23 @@ public class Jeu {
 
     }
 
-
+    //met à jour le joueur courant, passe au joueur suivant
     public void setJoueurCourant() {
         joueurCourant = (joueurCourant%nbJoueurs)+1;
     }
 
-    void finJoueur(int idJoueur){
-        listeJoueurs[idJoueur-1].setScoreFinal();
-        listeJoueurs[idJoueur-1].peutJouer = false;
-    }
-
-
+    //calcule les coins possible pour une piece pour la couleur courante d'un joueur
     private void calculeCoinPiece(Piece piece, int idJoueur) {
         Iterator<Case> it = piece.listeCases.iterator();
 
-        //ajoute les nouveaux coins possibles
+        //parcourt chaque case de la piece
         while (it.hasNext()){
             Case ca = it.next();
 
             int x = ca.getX();
             int y = ca.getY();
 
+            //pour les 4 coins potentiel, si il est valide, on l'ajoute à la liste des coins de la couleur
             estCoinValide(x+1,y-1,idJoueur);
             estCoinValide(x+1,y+1,idJoueur);
             estCoinValide(x-1,y-1,idJoueur);
@@ -107,7 +110,7 @@ public class Jeu {
 
         }
 
-        //reparcourt les coins pour le joueur courant et enleve les coins qui ne sont plus valides (meme couleurs à coté)
+        //reparcourt les coins de la couleur courante et enleve ceux qui nesont plus valides
         it = getJoueur(idJoueur).getCouleurCourante().listeCoins.iterator();
         while (it.hasNext()){
             Case ca = it.next();
@@ -116,6 +119,7 @@ public class Jeu {
             }
         }
 
+        //parcourt la liste des coins des autres couleurs et si notre piece ecrase un des coins de l'autre couleur on l'enleve
         it = piece.listeCases.iterator();
         while (it.hasNext()){
             Case ca = it.next();
@@ -131,6 +135,7 @@ public class Jeu {
 
     }
 
+    //true si un coin est valide (point dans la grille du niveau et n'as pas une meme couleur ajacente)
     boolean estCoinValide(int x, int y, int idJoueur){
         if(n.estDansGrille(x,y) && n.grille[x][y] == 0){
             if(n.aucunVoisin(x,y,getJoueur(idJoueur).getCouleurCourante().getId())){
@@ -142,18 +147,24 @@ public class Jeu {
     }
 
     //dans cette méthode on considère que la pièce est dans la grille et ne superpose aucune autre piece (grace à estPosable appellé avant)
+    //verifie si une piece est posable selon les regles du jeu
     public boolean estPosableRegle(LinkedList<Case> listeCasesPiece, int idJoueur){
         Couleur couleurCourante = getJoueur(idJoueur).getCouleurCourante();
         boolean estSurUnCoinPossible = false;
         Iterator<Case> it = listeCasesPiece.iterator();
+        //pour chaque case de la piece
         while (it.hasNext()){
             Case caCourante = it.next();
+            //on recupere la liste de voisins (case ajdjacente en "+")
             LinkedList<Case> voisinsCaseCourante = n.voisinsCase(caCourante);
             Iterator<Case> it2 = voisinsCaseCourante.iterator();
+            //pour chaque voisins
             while(it2.hasNext()){
                 Case voisin = it2.next();
+                //si le voisin est d'une meme couleur que notre couleur courante
+                // ou si notre piece contient déjà ce voisins(donc voisisn par réellement un voisin car est une case de la piece)
                 if(!(n.grille[voisin.getX()][voisin.getY()] != couleurCourante.getId() ||
-                        listeCasesPiece.contains(voisin))){ // && grille[voisin.getX()][voisin.getY()] == idJoueur)
+                        listeCasesPiece.contains(voisin))){
                     return false;
                 }
             }
@@ -163,7 +174,7 @@ public class Jeu {
 
         }
         //si on arrive ici : la piece n'est pas collée à une pièce de même couleur sur ses cotés "+"
-        //il suffit de retourner ok pour savoir si au moins une des cases de la piece est sur un coin possible pour le joueur
+        //il suffit de retourner "estSurUnCoinPossible" pour savoir si au moins une des cases de la piece est sur un coin possible pour le joueur
 
         return estSurUnCoinPossible;
     }
@@ -176,6 +187,7 @@ public class Jeu {
         return listeJoueurs[joueurCourant-1].getCouleurCourante().id;
     }
 
+    //traduit la matrice d'une piece en une liste de case par rapport à un point (x,y)
     public LinkedList<Case> tradMatrice(Piece p, int x, int y){
         int[][] matrice = p.getMatrice();
         LinkedList<Case> liste= new LinkedList<>();
@@ -192,27 +204,35 @@ public class Jeu {
         return liste;
     }
 
-    //si liste en retour à une taille de 0 alors aucune possiblités
+    //retourne une liste de CoupleListeValeur
+    //CoupleListeValeur contient une LinkedList<E> et une valeur
+    //retourne la liste de toute les position possible pour une piece selon toutes ses configurations (rotation, miroir)
+    //si la taille de la liste retourné est == 0 alors il n'y a aucune position possible pour cette piece (elle ne peut pas etre placé
     public LinkedList<CoupleListeValeur<Case,Integer>> positionPossibleConfig(Piece p){
 
         LinkedList<CoupleListeValeur<Case,Integer>> listeEmplacementPossible = new LinkedList<>();
 
+        //hashset pour stocker les configurations possibles pour une piece (differentes rotation/miroir peuvent obtenir la meme piece)
         HashSet<LinkedList<Case>> configPiecePossible = new HashSet<>();
         int decx,decy;
 
         LinkedList<Case> config;
 
-        for (int i=0;i<8;i++){ //chaque config
+        //pour chaque config (rotation,miroir)
+        for (int i=0;i<8;i++){
 
             decx = p.getDecx();
             decy = p.getDecy();
 
             config = tradMatrice(p,0,0);
 
+            //si cette configuration a déjà était testé/parcourut, on ne le refait pas
             if(!configPiecePossible.contains(config)){
 
+                //parcourt toute la grille du niveau
                 for (int x = 0;x<20;x++){
                     for (int y=0;y<20;y++){
+                        //si une piece est posable alors on stocke cette piece
                         if(n.estPosable(p,x-decx,y-decy)){
                             LinkedList<Case> configSelonEmplacement = tradMatrice(p,x-decx,y-decy);
                             if(estPosableRegle(configSelonEmplacement,this.joueurCourant)){
