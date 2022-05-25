506bf50c2d2543ca14567e78a223b46888190273
martinet <yo>
2022-05-25 12:46:34 +0200

    modification Configuration et utilisation de fichiers

--- src/Global/Configuration.java
+++ src/Global/Configuration.java
@@ -3,8 +3,6 @@ package Global;
 import java.io.*;
 import java.util.NoSuchElementException;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Configuration {
     private static Configuration instance = null;
@@ -17,12 +15,12 @@ public class Configuration {
     private Configuration() {
         prop = new Properties();
         try {
-            InputStream propIn = charge("config.cfg");
+            InputStream propIn = charge("defaut.cfg");
             prop.load(propIn);
-//            String home = System.getProperty("user.home");
-//            FileInputStream f = new FileInputStream(home + File.separator + ".kusblo");
-//            prop = new Properties(prop);
-//            prop.load(f);
+            String home = System.getProperty("user.home");
+            FileInputStream f = new FileInputStream(home + File.separator + ".kusblo");
+            prop = new Properties(prop);
+            prop.load(f);
         } catch (Exception e) {
             System.err.println("Erreur lors de la lecture de la configuration : " + e);
         }
@@ -54,7 +52,8 @@ public class Configuration {
     public void sauvegarde()
     {
         try{
-            FileOutputStream fr = new FileOutputStream("res/config.cfg");
+            String home = System.getProperty("user.home");
+            FileOutputStream fr = new FileOutputStream(home + File.separator + ".kusblo");
             prop.store(fr, null);
             fr.close();
         }catch(Exception e){
--- src/Modele/Sauvegarde.java
+++ src/Modele/Sauvegarde.java
@@ -2,6 +2,7 @@ package Modele;
 
 import Controleur.IA;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -11,7 +12,9 @@ public class Sauvegarde {
     public Sauvegarde(){}
 
     public void ecrire(Jeu j, IA[] ia){
-        String filename = "res/sauvegarde";
+        String home = System.getProperty("user.home");
+        String filename = home + File.separator + "save1";
+
         try {
             ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename));
             out.writeObject(j);
